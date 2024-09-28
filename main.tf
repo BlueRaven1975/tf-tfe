49a26c7da4c9b69ac2d81911421611c172c651ed
Romano Romano <romano.romano@gmail.com>
2024-09-28 16:35:23 +0200

    feat: connect the tf-tfe workspace to GitHub

--- main.tf
+++ main.tf
@@ -64,6 +64,12 @@ resource "tfe_workspace" "tf_tfe" {
   description         = "Manage my Terraform Cloud instance with Terraform"
   global_remote_state = true
   name                = "tf-tfe"
+
+  vcs_repo {
+    identifier     = "BlueRaven1975/tf-tfe"
+    oauth_token_id = tfe_oauth_client.this.oauth_token_id
+  }
+
 }
 
 # Workspaces Settings
