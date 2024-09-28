3dbf2a8719731c86f58d39293df57ef378fb9e27
Romano Romano <romano.romano@gmail.com>
2024-09-28 16:16:27 +0200

    feat: add OAuth Client for GitHub

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# OAuth Client
+resource "tfe_oauth_client" "this" {
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  name             = "GitHub"
+  oauth_token      = var.github_token
+  service_provider = "github"
+}
+
 # Organization
 resource "tfe_organization" "this" {
   cost_estimation_enabled = true
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "github_token" {
+  description = "GitHub API token"
+  sensitive   = true
+  type        = string
+}
+
 variable "tfe_token" {
   description = "Terraform Cloud API token"
   sensitive   = true
