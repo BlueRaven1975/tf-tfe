fc117eb5ce28f4e5b637f7861e715f4def98993d
Romano Romano <romano.romano@gmail.com>
2024-09-28 16:30:38 +0200

    feat: connect the tf-local-k8s-cluster workspace to GitHub

--- main.tf
+++ main.tf
@@ -52,6 +52,12 @@ resource "tfe_workspace" "tf_local_k8s_cluster" {
   description         = "Setup and configure a local K8s cluster with Terraform"
   global_remote_state = true
   name                = "tf-local-k8s-cluster"
+
+  vcs_repo {
+    identifier     = "BlueRaven1975/tf-local-k8s-cluster"
+    oauth_token_id = tfe_oauth_client.this.oauth_token_id
+  }
+
 }
 
 resource "tfe_workspace" "tf_tfe" {
