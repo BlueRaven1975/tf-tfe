adc1de6377b11a918ab8f736013d7e9bba06e4ef
Romano Romano <romano.romano@gmail.com>
2024-09-28 16:28:52 +0200

    feat: connect the tf-argocd-apps workspace to GitHub

--- main.tf
+++ main.tf
@@ -28,6 +28,12 @@ resource "tfe_workspace" "tf_argocd_apps" {
   description         = "Deploy ArgoCD Applications manifests on my local K8s cluster with Terraform"
   global_remote_state = true
   name                = "tf-argocd-apps"
+
+  vcs_repo {
+    identifier     = "BlueRaven1975/tf-argocd-apps"
+    oauth_token_id = tfe_oauth_client.this.oauth_token_id
+  }
+
 }
 
 resource "tfe_workspace" "tf_github" {
