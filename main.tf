5edad9f95da84b15407358a124980a75f2a2c489
Romano Romano <romano.romano@gmail.com>
2024-10-05 12:09:02 +0200

    feat: add tf-aws workspace

--- main.tf
+++ main.tf
@@ -43,6 +43,19 @@ resource "tfe_workspace" "tf_argocd_apps" {
 
 }
 
+resource "tfe_workspace" "tf_aws" {
+  description         = "Manage my AWS infrastructure with Terraform"
+  global_remote_state = true
+  name                = "tf-aws"
+  terraform_version   = "~> 1.9.0"
+
+  vcs_repo {
+    identifier     = "BlueRaven1975/tf-aws"
+    oauth_token_id = tfe_oauth_client.this.oauth_token_id
+  }
+
+}
+
 resource "tfe_workspace" "tf_github" {
   description         = "Manage my GitHub repositories via Terraform"
   global_remote_state = true
