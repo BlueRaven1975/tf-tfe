b329a7d7b08e1d35822480292fb6aa331df08dca
Romano Romano <romano.romano@gmail.com>
2024-10-18 11:59:17 +0200

    feat: add tf-aws workspace variables

--- tf-aws.tf
+++ tf-aws.tf
@@ -1,3 +1,20 @@
+# Workspace Variables
+resource "tfe_variable" "aws_access_key_id" {
+  category     = "terraform"
+  description  = "Terraform Cloud service account AWS access key ID"
+  key          = "aws_access_key_id"
+  sensitive    = true
+  workspace_id = tfe_workspace.tf_aws.id
+}
+
+resource "tfe_variable" "aws_secret_access_key" {
+  category     = "terraform"
+  description  = "Terraform Cloud service account AWS secret access key"
+  key          = "aws_secret_access_key"
+  sensitive    = true
+  workspace_id = tfe_workspace.tf_aws.id
+}
+
 # Workspace
 resource "tfe_workspace" "tf_aws" {
   description         = "Manage my AWS infrastructure with Terraform"
