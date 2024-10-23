ac3889b6b2f6e7875a77b5e43521066caac978b3
Romano Romano <romano.romano@gmail.com>
2024-10-23 16:30:35 +0200

    feat: add tf-new-relic workspace

---
+++ tf-new-relic.tf
@@ -0,0 +1,22 @@
+# Workspace Variables
+resource "tfe_variable" "new_relic_api_key" {
+  category     = "terraform"
+  description  = "Terraform Cloud (Full Access)"
+  key          = "new_relic_api_key"
+  sensitive    = true
+  workspace_id = tfe_workspace.tf_aws.id
+}
+
+# Workspace
+resource "tfe_workspace" "tf_new_relic" {
+  description         = "Manage my New Relic instance with Terraform"
+  global_remote_state = true
+  name                = "tf-new-relic"
+  terraform_version   = "~> 1.9.0"
+
+  vcs_repo {
+    identifier     = "BlueRaven1975/tf-new-relic"
+    oauth_token_id = tfe_oauth_client.this.oauth_token_id
+  }
+
+}
